fac3188db286da1ea9827ed2b8e9f45f5a68019a
Alonso <alonsoz1211@hotmail.com>
2018-05-19 18:45:01 +0200

    18:44

---
+++ routes/credits.js
@@ -0,0 +1,10 @@
+var express = require('express');
+//var path = require('path');
+
+var route = express.Router();
+
+route.get('/',function(req,res){
+    res.render('credits');
+});
+
+module.exports = route;
---
+++ routes/quizzes.js
@@ -0,0 +1,15 @@
+var express = require ('express');
+var router = express.Router();
+
+const Sequelize = require('sequelize');
+const {models} = require('../models/index');
+
+router.get('/', function(req,res,next){
+    models.quiz.findAll()
+        .then(quizzes=>{
+        res.render('quizzes',{quiz: `${quizzes}`});
+})
+.catch(error => next(error));
+});
+
+module.exports = router;
